72abb91ebd56d21e00a77cc423c8f22d68b3668d
Zhang <kzb11128@gmail.com>
2023-07-14 15:21:31 -0400

    added login, logout and profile to public

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,7 +51,7 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -61,7 +61,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
+    res.render('profile', {
       ...user,
       logged_in: true
     });
@@ -73,7 +73,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/profile');
     return;
   }
 
